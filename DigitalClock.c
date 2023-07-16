346e113031c4b3b77d9b5eb5f5b2c32f4a989c5b
= <b122079@iiit-bh.ac.in>
2023-07-16 09:23:20 +0530

    Bug , If User input invalid time

--- DigitalClock.c
+++ DigitalClock.c
@@ -12,21 +12,52 @@ int main()
 
     printf("Enter Year: ");
     scanf("%d", &year);
+    
+       if (year < 0)
+    {
+        printf("Invalid year please ReEnter :");
+        scanf("%d", &year);
+    }
 
     printf("Enter Month: ");
     scanf("%d", &month);
+    if (month > 12 || month < 0)
+    {
+        printf("Invalid month please ReEnter :");
+        scanf("%d", &month);
+    }
 
     printf("Enter Date: ");
     scanf("%d", &date);
+    if (date > 31 || date < 0)
+    {
+        printf("Invalid date please ReEnter :");
+        scanf("%d", &date);
+    }
 
     printf("Enter Hour: ");
     scanf("%d", &hour);
+    if (hour > 24 || hour < 0)
+    {
+        printf("Invalid hour please ReEnter :");
+        scanf("%d", &hour);
+    }
 
     printf("Enter Minute's: ");
     scanf("%d", &minute);
+    if (minute > 60 || minute < 0)
+    {
+        printf("Invalid minute please ReEnter :");
+        scanf("%d", &minute);
+    }
 
     printf("Enter Second: ");
     scanf("%d", &second);
+    if (second > 60 || second < 0)
+    {
+        printf("Invalid second please ReEnter :");
+        scanf("%d", &second);
+    }
 
     while (1)
     {
@@ -34,7 +65,7 @@ int main()
         {
             printf("\t\t\t\t\tDATE MONTH YEAR\n", hour, minute, second);
             printf("\t\t\t\t\t%02d : %02d : %02d  \n", date, month, year);
-            printf("\t\t\t\t\t%02d:%02d:%02d  AM\n", hour, minute, second);
+            printf("\t\t\t\t\t%02d:%02d:%02d  AM \n", hour, minute, second);
         }
         else
         {
